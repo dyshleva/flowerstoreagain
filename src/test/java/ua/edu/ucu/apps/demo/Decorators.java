ec61beecea34f4921751bd08a930de533ffd87c0
dyshleva <116552489+dyshleva@users.noreply.github.com>
2023-11-09 18:52:25 +0200

    Init commit 5

--- src/test/java/ua/edu/ucu/apps/demo/Decorators.java
+++ src/test/java/ua/edu/ucu/apps/demo/Decorators.java
@@ -1,6 +1,6 @@
 package ua.edu.ucu.apps.demo;
 
-import org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 import ua.edu.ucu.apps.demo.decorators.PaperDecorator;
 import ua.edu.ucu.apps.demo.decorators.RibbonDecorator;
